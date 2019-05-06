4ea903279a5cafa508415ee206b60e227064ec87
Lucia Cavagna <cavagnalucia@gmail.com>
2019-05-05 21:38:52 -0300

    explicacion de lo que funciona en DHM para setear la antiguedad EN GENERAL

--- DH-Manager/Classes/DigitalHouseManager.php
+++ DH-Manager/Classes/DigitalHouseManager.php
@@ -42,14 +42,20 @@ class DigitalHouseManager
     public function getListaProfesores():array{
         return $this->listaProfesores;
     }
-    
+
     public function setListaProfesores(array $pollito, int $pollo){
         foreach($this->listaProfesores as $profesor){
             $profesor->setantiguedad($pollo);
         }
     } 
     
- 
+    // si yo hago esto, funciona, pero cambia todas las antiguedad de todos los profesores dentro de la lista de profesores... por eso, lo de arriba, hay que acceder al array en la posicion en donde se quiere cambiar la antiguedad.
+/*     public function setListaProfesores(int $pollo){
+        foreach($this->listaProfesores as $profesor){
+            $profesor->setantiguedad($pollo);
+        }
+    } 
+  */
 
 
 }
